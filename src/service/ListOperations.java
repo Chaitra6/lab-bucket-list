5c989e1516b41fc6d47df3dc5cfda7fa1c6dfeb6
Chaitra6 <48185350+Chaitra6@users.noreply.github.com>
2020-09-06 22:26:29 +0530

    FSJ0820A003

--- src/service/ListOperations.java
+++ src/service/ListOperations.java
@@ -1,1 +1,39 @@
 package service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import model.TouristPlace;
+
+public class ListOperations{
+	static List<TouristPlace> list=new ArrayList<TouristPlace>();
+	
+    public List<TouristPlace> add(TouristPlace places) {    	
+    	list.add(places);    
+    	return list;
+   }
+    
+    public List<TouristPlace> remove(TouristPlace places) {    	
+    	list.remove(places);    
+    	return list;    	
+    }
+    
+    public Object sortByDestination(List places) {
+    	
+        places.sort((TouristPlace p1,TouristPlace p2)-> p1.getDestination().compareTo(p2.getDestination()));
+        return places;
+    }
+    
+    public Object sortByRank(List places) {
+    	places.sort((TouristPlace p1,TouristPlace p2)-> p1.getRank().compareTo(p2.getRank()));
+        return places;
+    }
+    
+    public Object reset(List places) {
+    	places.clear();
+    	return places;
+    }
+    
+    
+    
+}
--- src/service/MapOperations.java
+++ src/service/MapOperations.java
@@ -1,2 +1,24 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import model.TouristPlace;
+
+public class MapOperations{
+    Map<Integer,String> map = new HashMap<Integer,String>();
+    
+	public Map<Integer,String> add(TouristPlace places) {
+		 for(int i=0; i< map.size();i++) {
+			 map.put(i, places);
+			 return map;
+		 }		 
+	}
+	
+	
+}
+
+ 
